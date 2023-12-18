5c58ebe46d26047818f8e4350e2122458b2e74c2
guilhermedel <glhrmgithub@gmail.com>
2023-12-18 00:38:58 -0300

    feat: creating about component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { images } from 'src/app/fakeImages/fakeImages';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.css'],
 })
 export class SliderComponent {
+  images: string[] = images;
+  selectedIndex: number = 0;
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.selectedIndex = this.selectedIndex + 1;
+      if (this.selectedIndex === this.images.length) {
+        this.selectedIndex = 0;
+      }
+    }, 3000);
+  }
+
+  selectImage(i: number) {
+    this.selectedIndex = i;
+  }
 }
---
+++ src/app/fakeImages/fakeImages.ts
@@ -0,0 +1,6 @@
+export const images = [
+  'assets/paisagem-1.avif',
+  'assets/paisagem-2.avif',
+  'assets/paisagem-3.avif',
+  'assets/paisagem-4.avif',
+];
